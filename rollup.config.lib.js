9dc437c6b54a47255b2055c6938c3d00c46e2035
Iurii Panarin <iurii.panarin@mcmakler.de>
2020-03-28 14:11:03 +0100

    feat(build)

---
+++ rollup.config.lib.js
@@ -0,0 +1,52 @@
+import copy from 'rollup-plugin-copy';
+import pkg from './package.json';
+import { terser } from "rollup-plugin-terser";
+import typescript from "rollup-plugin-typescript2";
+
+export default [
+    {
+        input: "src/index.ts",
+        output: {
+            file: "./lib/hasty.umd.js",
+            format: 'umd',
+            name: 'hasty',
+        },
+        plugins: [
+            typescript({
+                tsconfig: 'tsconfig.lib.json'
+            }),
+            terser(),
+            copy({
+                "./package.json": "./lib/package.json",
+                "./README.md": "./lib/README.md",
+                './LICENSE': './lib/LICENSE',
+                verbose: true
+            }),
+        ],
+    },
+    {
+        input: "src/index.ts",
+        output: [
+			{ file: './lib/' + pkg.module, format: 'es' }
+		],
+        plugins: [
+            typescript(),
+        ],
+    },
+    {
+        input: "src/index.ts",
+        output: [
+			{ file: './lib/' + pkg.es2015, format: 'es' }
+		],
+        plugins: [
+            typescript({
+                tsconfig: 'tsconfig.lib.json',
+                tsconfigOverride: {
+                    compilerOptions: {
+                        target: "es2015"
+                    }
+                }
+            }),
+        ],
+    },
+]
